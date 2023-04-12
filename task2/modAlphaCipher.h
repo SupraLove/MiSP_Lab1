e4f958185ae293ea19a9958f72ba33da255faf9f
Ramil ♥ <114113891+SupraLove@users.noreply.github.com>
2023-04-12 15:37:47 +0300

    Update modAlphaCipher.h

--- task2/modAlphaCipher.h
+++ task2/modAlphaCipher.h
@@ -1,21 +1,13 @@
 #pragma once
-#include <iostream>
 #include <string>
-#include <vector>
-#include <map>
-#include <cctype>
-#include <locale>
-#include <codecvt>
-#include <algorithm>
 using namespace std;
-
-class modAlphaCipher
+class Cipher
 {
 private:
-    int newkey; // количество столбцов таблицы
+    int p;
 public:
-    modAlphaCipher()=delete;
-    modAlphaCipher(const int& key) :newkey(key) {};   // конструктор
-    string coder(const string& open_st);   // зашифрование
-    string decoder(const string& cipher_st); // расшифрование 
+    Cipher()=delete;
+    Cipher(int w);
+    wstring zakodirovatCipher(Cipher w, wstring& s);
+    wstring raskodirovatCipher(Cipher w, wstring& s);
 };
