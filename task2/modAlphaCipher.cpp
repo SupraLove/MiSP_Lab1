a50aa1f891e225c83520437fc29ba80e4c2f4709
Ramil ♥ <114113891+SupraLove@users.noreply.github.com>
2023-04-12 15:37:22 +0300

    Update modAlphaCipher.cpp

--- task2/modAlphaCipher.cpp
+++ task2/modAlphaCipher.cpp
@@ -1,38 +1,56 @@
 #include "modAlphaCipher.h"
-
-string modAlphaCipher::decoder(const std::string& cipher_st) // РАСШИФРОВАНИЕ
+Cipher::Cipher(int password)
 {
-    string n_s = cipher_st;
-    int dlina, nstrok, position, a; // dlina-длина строки ; nstrok-количество строк ; position-новая позиция символа, который меняем ;
-    dlina = cipher_st.size(); // st-строка с текстом, который вводится пользователем
-    nstrok = (dlina - 1) / newkey + 1; // newkey-количество столбцов в таблице
-    a = 0;
-    for (int colum_number = newkey; colum_number > 0; colum_number--) { // colum_number-номер столбца
-        for (int line_number = 0; line_number < nstrok; line_number++) { // line_number-номер строки
-            position = newkey*line_number+colum_number; //
-            if (position-1 < dlina) {
-                n_s[position-1] = cipher_st[a];
-                a++;
-            }
-        }
-    }
-    return n_s;
+    this->p=password;
 }
-string modAlphaCipher::coder(const string& open_st) // ЗАШИФРОВАНИЕ
+wstring Cipher::zakodirovatCipher(Cipher w, wstring& s)
 {
-    string n_s = open_st;
-    int dlina, nstrok, position, a; // dlina-длина строки ; nstrok-количество строк ; position-новая позиция символа, который меняем ;
-    dlina = open_st.size(); // st-строка с текстом, который вводится пользователем
-    nstrok = (dlina - 1) / newkey + 1; //newkey-количество столбцов в таблице
-    a = 0;
-    for (int colum_number = newkey; colum_number > 0; colum_number--) { // colum_number-номер столбца
-        for (int line_number = 0; line_number < nstrok; line_number++) { // line_number-номер строки
-            position = colum_number+newkey*line_number;
-            if (position-1 < dlina) {
-                n_s[a] = open_st[position-1];
-                a++;
-            }
+    wstring Output;
+    int v;
+    int dlina=s.size();
+    if (s.size()%w.p!=0) {
+        v=s.size()/w.p+1;
+    } else {
+        v=s.size()/w.p;
+    }
+    wchar_t x[v][w.p];
+    int p=0;
+    for (int i=0; i<v; ++i) {
+        for (int k=0; k<w.p; ++k) {
+            if (p<s.length()) {
+                x[i][k]=s[p];
+                ++p;
+            } else x[i][k]=' ';
         }
     }
-    return n_s;
+    for (int i=0; i<w.p; ++i) {
+        for (int k=0; k<v; ++k) {
+            Output+=x[k][i];
+        }
+    }
+    return Output;
+}
+wstring Cipher::raskodirovatCipher(Cipher w, wstring& s)
+{
+    int v;
+    if (s.size()%w.p!=0) {
+        v=s.size()/w.p+1;
+    } else {
+        v=s.size()/w.p;
+    }
+    wchar_t x[v][w.p];
+    int p=0;
+    for (int i=0; i<w.p; ++i) {
+        for (int k=0; k<v; ++k) {
+            x[k][i]=s[p];
+            ++p;
+        }
+    }
+    wstring deOutput;
+    for (int i=0; i<v; ++i) {
+        for (int k=0; k<w.p; ++k) {
+            deOutput+=x[i][k];
+        }
+    }
+    return deOutput;
 }
