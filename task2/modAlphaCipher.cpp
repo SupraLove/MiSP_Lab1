92b0d5c008dc6e44c7fc053e2eddc0ab45ab01ad
Ramil ♥ <114113891+SupraLove@users.noreply.github.com>
2023-04-16 17:11:24 +0300

    Update modAlphaCipher.cpp

--- task2/modAlphaCipher.cpp
+++ task2/modAlphaCipher.cpp
@@ -1,8 +1,10 @@
 #include "modAlphaCipher.h"
+
 Cipher::Cipher(int password)
 {
-    this->p=password;
+    this->p = password;
 }
+
 wstring Cipher::zakodirovatCipher(Cipher w, wstring& s)
 {
     wstring Output;
@@ -20,12 +22,14 @@ wstring Cipher::zakodirovatCipher(Cipher w, wstring& s)
             if (p < s.length()) {
                 x[i][k] = s[p];
                 ++p;
-            } else x[i][k] = ' ';
+            } else {
+                x[i][k] = ' ';
+            }
         }
     }
-    for (int i = w.p - 1; i >= 0; --i) { // изменяем направление обхода
+    for (int i = 0; i < w.p; ++i) {
         for (int k = 0; k < v; ++k) {
-            Output += x[k][i]; // добавляем символ в выходную строку
+            Output += x[k][i]; 
         }
     }
     return Output;
@@ -33,9 +37,7 @@ wstring Cipher::zakodirovatCipher(Cipher w, wstring& s)
 
 wstring Cipher::raskodirovatCipher(Cipher w, wstring& s)
 {
-    wstring Output;
     int v;
-    int dlina = s.size();
     if (s.size() % w.p != 0) {
         v = s.size() / w.p + 1;
     } else {
@@ -43,16 +45,17 @@ wstring Cipher::raskodirovatCipher(Cipher w, wstring& s)
     }
     wchar_t x[v][w.p];
     int p = 0;
-    for (int i = w.p - 1; i >= 0; --i) {
+    for (int i = 0; i < w.p; ++i) {    // изменяем направление обхода
         for (int k = 0; k < v; ++k) {
-            x[k][i] = s[p]; // записываем символ в текущую ячейку
+            x[k][i] = s[p];  // исправлено
             ++p;
         }
     }
+    wstring deOutput;
     for (int i = 0; i < v; ++i) {
         for (int k = 0; k < w.p; ++k) {
-            Output += x[i][k]; // добавляем символ в выходную строку
+            deOutput += x[i][k];
         }
     }
-    return Output;
+    return deOutput;
 }
