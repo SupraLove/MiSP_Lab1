1333c73d6f019ac7120af6e25e2e9f15e9c51d53
Ramil ♥ <114113891+SupraLove@users.noreply.github.com>
2023-04-16 16:37:05 +0300

    Update modAlphaCipher.cpp

--- task2/modAlphaCipher.cpp
+++ task2/modAlphaCipher.cpp
@@ -6,26 +6,26 @@ Cipher::Cipher(int password)
 wstring Cipher::zakodirovatCipher(Cipher w, wstring& s)
 {
     wstring Output;
-    int v;
-    int dlina=s.size();
-    if (s.size()%w.p!=0) {
-        v=s.size()/w.p+1;
-    } else {
-        v=s.size()/w.p;
+int v;
+int dlina = s.size();
+if (s.size() % w.p != 0) {
+    v = s.size() / w.p + 1;
+} else {
+    v = s.size() / w.p;
+}
+wchar_t x[v][w.p];
+int p = 0;
+for (int i = 0; i < v; ++i) {
+    for (int k = 0; k < w.p; ++k) {
+        if (p < s.length()) {
+            x[i][k] = s[p];
+            ++p;
+        } else x[i][k] = ' ';
     }
-    wchar_t x[v][w.p];
-    int p=0;
-    for (int i=0; i<v; ++i) {
-        for (int k=0; k<w.p; ++k) {
-            if (p<s.length()) {
-                x[i][k]=s[p];
-                ++p;
-            } else x[i][k]=' ';
-        }
-    }
-    for (int i=0; i<w.p; ++i) {
-        for (int k=0; k<v; ++k) {
-            Output+=x[k][i];
+}
+for (int i = w.p - 1; i >= 0; --i) { // изменяем направление обхода
+    for (int k = 0; k < v; ++k) {
+        Output += x[k][i]; // добавляем символ в выходную строку
         }
     }
     return Output;
